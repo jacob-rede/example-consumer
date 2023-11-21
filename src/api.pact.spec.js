abe929a98ca62dadf8ccfee5d11097f99c656a28
Jacob Rede <jacobrede@gmail.com>
2023-11-20 22:27:31 -0800

    feat: remove color

--- src/api.pact.spec.js
+++ src/api.pact.spec.js
@@ -20,7 +20,7 @@ describe('API Pact test', () => {
         id: '10',
         type: 'CREDIT_CARD',
         name: '28 Degrees',
-        color: 'red'
+        flag: true
       };
 
       // Uncomment to see this fail
